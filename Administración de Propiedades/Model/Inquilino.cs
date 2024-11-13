d987932d8ec7e46d1598e72598af0eaa929726c3
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-11-13 06:49:27 -0500

    se crea el modelo Inquilino

---
+++ Administración de Propiedades/Model/Inquilino.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace Administración_de_Propiedades.Model
+{
+    public class Inquilino
+    {
+        [Key]
+        public int IdInquilino { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Apellido { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string Telefono { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        // Relación muchos a uno con Propiedad
+        public int IdPropiedad { get; set; }
+        [ForeignKey("IdPropiedad")]
+        public Propiedad Propiedad { get; set; }
+
+        // Relación uno a muchos con Contratos
+        public ICollection<Contrato> Contratos { get; set; }
+    }
+}
