d39fe427b04c36fb847d1d8667d586689c8f92da
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-11-13 06:45:05 -0500

    creamos las carpetas correspondientes y creamos el primer modelo propiedad

---
+++ Administración de Propiedades/Model/Propiedad.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Administración_de_Propiedades.Model
+{
+    public class Propiedad
+    {
+        [Key]
+        public int IdPropiedad { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Direccion { get; set; }
+
+        [Required]
+        public string TipoPropiedad { get; set; }
+
+        [Required]
+        public int NumeroHabitaciones { get; set; }
+
+        [Required]
+        public decimal PrecioAlquiler { get; set; }
+
+        [Required]
+        public bool Disponible { get; set; } 
+
+        // Relación uno a muchos con Inquilinos
+        public ICollection<Inquilino> Inquilinos { get; set; }
+
+        // Relación uno a muchos con Contratos
+        public ICollection<Contrato> Contratos { get; set; }
+    }
+}
