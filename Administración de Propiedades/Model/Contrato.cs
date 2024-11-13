a7d510784ea8d9c0d15b335935aa48b88e50245b
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-11-13 06:56:23 -0500

    se crea el modelo de pago

--- Administración de Propiedades/Model/Contrato.cs
+++ Administración de Propiedades/Model/Contrato.cs
@@ -1,6 +1,37 @@
-﻿namespace Administración_de_Propiedades.Model
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Administración_de_Propiedades.Model
 {
     public class Contrato
     {
+        [Key]
+        public int IdContrato { get; set; }
+
+        // Relación muchos a uno con Propiedad
+        public int IdPropiedad { get; set; }
+        [ForeignKey("IdPropiedad")]
+        public Propiedad Propiedad { get; set; }
+
+        // Relación muchos a uno con Inquilino
+        public int IdInquilino { get; set; }
+        [ForeignKey("IdInquilino")]
+        public Inquilino Inquilino { get; set; }
+
+        [Required]
+        public DateTime FechaInicio { get; set; }
+
+        [Required]
+        public DateTime FechaFin { get; set; }
+
+        [Required]
+        public decimal Deposito { get; set; }
+
+        [Required]
+        public string Terminos { get; set; }
+
+        // Relación uno a muchos con Pagos
+        public ICollection<Pago> Pagos { get; set; }
     }
 }
---
+++ Administración de Propiedades/Model/Pago.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Administración_de_Propiedades.Model
+{
+    public class Pago
+    {
+        [Key]
+        public int IdPago { get; set; }
+
+        // Relación muchos a uno con Contrato
+        public int IdContrato { get; set; }
+        [ForeignKey("IdContrato")]
+        public Contrato Contrato { get; set; }
+
+        [Required]
+        public DateTime FechaPago { get; set; }
+
+        [Required]
+        public decimal Monto { get; set; }
+
+        [Required]
+        public string Estado { get; set; }
+    }
+}
