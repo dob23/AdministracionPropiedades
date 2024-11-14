ec2119878e7369748fbfac903328e632728978da
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-11-13 22:28:33 -0500

    creamos en el data la clase propiedadescontext

---
+++ Administración de Propiedades/Data/PropiedadesContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Administración_de_Propiedades.Model;
+namespace Administración_de_Propiedades.Data
+{
+    public class PropiedadesContext : DbContext
+    {
+        public PropiedadesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
