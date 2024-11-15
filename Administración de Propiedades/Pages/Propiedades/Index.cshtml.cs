c74ac4edebdea06f544bc087444a91a0ba2e0ee9
Diana Perez <dianaperezc10@gmail.com>
2024-11-15 10:35:12 -0500

    creacion de la pagina propiedades y se hace la migracion de la base de datos de la rama daniel_ortiz

---
+++ Administración de Propiedades/Pages/Propiedades/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Administración_de_Propiedades.Pages.Propiedades
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
