6856e6cd2801c4637dd6a1b82fdca342959b09e9
Diana Perez <dianaperezc10@gmail.com>
2024-11-15 10:50:25 -0500

    creacion del index de propiedades

--- Administración de Propiedades/Pages/Propiedades/Index.cshtml.cs
+++ Administración de Propiedades/Pages/Propiedades/Index.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Administración_de_Propiedades.Data;
+using Administración_de_Propiedades.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Administración_de_Propiedades.Pages.Propiedades
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly PropiedadesContext _context;
+
+        public IndexModel(PropiedadesContext context)
         {
+            _context = context;
+        }
+
+        public IList<Propiedad> Propiedades { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Propiedades != null)
+            {
+                Propiedades = await _context.Propiedades.ToListAsync();
+            }
         }
     }
 }
